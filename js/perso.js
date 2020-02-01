791b2d2c2f5c07c283935b5dc0ad9be4b63fde97
Nada <sign.adrien@gmail.com>
2020-02-01 11:46:18 +0100

    Ajout de repair

--- js/perso.js
+++ js/perso.js
@@ -1,83 +1,79 @@
 class Perso {
 
-HOLDING={
-		EMPTY : 'empty',
-		WOOD : 'wood',
-		IRON : 'iron',
-		EXTINGUISHER: 'extinguisher'
-	};
-
-ORIENTATION={
-		RIGHT : 'right',
-		LEFT : 'left',
-		BACK : 'back'
-	};
-	pos = null;
-	holdType=this.HOLDING.EMPTY;
-	direction=this.ORIENTATION.RIGHT;
+    ORIENTATION = {
+        RIGHT: 'right',
+        LEFT: 'left',
+        BACK: 'back'
+    };
+    pos = null;
+    holdType = material.EMPTY;
+    direction = this.ORIENTATION.RIGHT;
 
 
+    constructor(x, y) {
+        this.pos = new Position(x, y);
 
-	constructor(x , y){
-		this.pos = new Position(x,y);
+    }
 
-	}
+    get pos() {
+        return this.pos;
+    }
 
-	get pos() {
-		return this.pos;
-	}
+    get holdObject() {
+        return this.holdObject;
+    }
 
-	get holdObject(){
-		return this.holdObject;
-	}
-
-	get direction(){
-		return this.direction;
-	}
+    get direction() {
+        return this.direction;
+    }
 
 
-	goLeft(){
-		this.pos.x-=1;
-		this.direction=this.ORIENTATION.LEFT;
-	}
+    goLeft() {
+        this.pos.x -= 1;
+        this.direction = this.ORIENTATION.LEFT;
+    }
 
-	goRight(){
-		this.pos.x+=1;
-		this.direction=this.ORIENTATION.RIGHT;
-	}
+    goRight() {
+        this.pos.x += 1;
+        this.direction = this.ORIENTATION.RIGHT;
+    }
 
-	goUp(){
-		this.pos.y-=1;
-		this.direction=this.ORIENTATION.BACK;
-	}
+    goUp() {
+        this.pos.y -= 1;
+        this.direction = this.ORIENTATION.BACK;
+    }
 
-	goDown(){
-		this.pos.y+=1;
-		this.direction=this.ORIENTATION.BACK;
-	}
+    goDown() {
+        this.pos.y += 1;
+        this.direction = this.ORIENTATION.BACK;
+    }
 
-	takeObject(obj){
-		if(this.holdType!=this.HOLDING.EMPTY){
-			//faire quelque chose avec l'objet tenu
-		}
-		switch(obj.type){
-			case "wood":
-			//change sprite to wood holder
-			this.holdType=this.HOLDING.WOOD;
-			break;
-			case "iron":
-			//change sprite to iron holder
-			this.holdType=this.HOLDING.IRON;
-			break;
-			case "extinguisher" :
-			default:
-			break;
+    takeObject(obj) {
+        if (this.holdType != material.EMPTY) {
+            //faire quelque chose avec l'objet tenu
+        }
+        switch (obj.type) {
+            case "wood":
+                //change sprite to wood holder
+                this.holdType = material.WOOD;
+                break;
+            case "iron":
+                //change sprite to iron holder
+                this.holdType = material.IRON;
+                break;
+            case "extinguisher" :
+            default:
+                break;
 
-		}
-	}
+        }
+    }
 
-	repair(anoma){
+    repair(anoma) {
 
-	}
+        if (this.holdType === anoma.type) {
+			anoma.repaired();
+        }
+
+    }
 
 }
